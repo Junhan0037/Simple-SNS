c3368f76d3912c76981a14773319be69ed8433d3
Junhan0037 <wnsgks0037@naver.com>
2024-12-16 23:17:43 +0900

    fix: exception 주석 추가

--- src/main/java/com/simplesns/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/simplesns/exception/CustomAuthenticationEntryPoint.java
@@ -8,13 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint { // // Spring Security 에서 인증 실패 시 호출되는 EntryPoint 를 정의
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.setContentType("application/json");
-        response.setStatus(ErrorCode.INVALID_TOKEN.getStatus().value());
-        response.getWriter().print(ErrorCode.INVALID_TOKEN.getMessage());
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException { // 인증 실패 시 호출되는 메서드로, 클라이언트에 에러 응답을 보냄
+        response.setContentType("application/json"); // 응답의 콘텐츠 타입을 JSON 으로 설정
+        response.setStatus(ErrorCode.INVALID_TOKEN.getStatus().value()); // 응답 상태 코드를 에러 코드에서 정의된 상태 코드로 설정
+        response.getWriter().print(ErrorCode.INVALID_TOKEN.getMessage()); // 응답 본문에 에러 메시지를 작성
     }
 
 }
--- src/main/java/com/simplesns/exception/SimpleSnsApplicationException.java
+++ src/main/java/com/simplesns/exception/SimpleSnsApplicationException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class SimpleSnsApplicationException extends RuntimeException {
+public class SimpleSnsApplicationException extends RuntimeException { // 사용자 정의 예외 클래스 정의 (RuntimeException 을 상속받음)
 
     private ErrorCode errorCode;
     private String message;
@@ -16,7 +16,7 @@ public class SimpleSnsApplicationException extends RuntimeException {
     }
 
     @Override
-    public String getLocalizedMessage() {
+    public String getLocalizedMessage() { // 런타임 예외가 사용자에게 출력될 때 호출
         if (message == null) {
             return errorCode.getMessage();
         } else {
