ecba01dfaee416e5b592fa26f3acfae59b0f9aad
Evan0037 <evan@planit.ai>
2024-12-19 14:13:48 +0900

    fix: 주석

--- src/main/java/com/simplesns/kafka/AlarmProducer.java
+++ src/main/java/com/simplesns/kafka/AlarmProducer.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+/**
+ * Kafka 를 사용하여 알람 이벤트를 프로듀싱(송신)하는 클래스
+ */
 @Component
 @RequiredArgsConstructor
 @Slf4j
@@ -18,6 +21,7 @@ public class AlarmProducer {
     private String topic;
 
     public void send(AlarmEvent event) {
+        // 지정된 토픽에 이벤트를 전송
         alarmEventKafkaTemplate.send(topic, event.getReceiverUserId(), event);
         log.info("send fin");
     }
